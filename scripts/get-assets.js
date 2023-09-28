8dddb44454bcfc6bc82fe3cf14b06e50c072d856
Shreyas <shreyass.ranganatha@gmail.com>
2023-09-28 14:37:53 +0530

    Add comments and refactor code
    Signed-off-by: Shreyas <shreyass.ranganatha@gmail.com>

--- scripts/get-assets.js
+++ scripts/get-assets.js
@@ -1,9 +1,10 @@
 import https from "https";
 import fs from "fs";
 
+// required assets assumed to be hosted in the `assets` branch of the below repository
 const repo = "falcosecurity/falco-playground"
 
-https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) => {
+https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) => {  // Get the latest version
     if (res.statusCode != 200) {
         console.error(`latest.txt: ${res.statusCode} - ${res.statusMessage}`);
         return;
@@ -12,6 +13,7 @@ https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) =
     res.on('data', function (version) {
         console.log(`Latest version: ${version}`)
 
+        // falco.wasm
         https.get(`https://raw.githubusercontent.com/${repo}/assets/${version}/falco.wasm`, (res) => {
             if (res.statusCode != 200) {
                 console.error(`falco.wasm: ${res.statusCode} - ${res.statusMessage}`);
@@ -19,8 +21,8 @@ https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) =
             }
 
             const path = `public/falco.wasm`;
-            const filePath = fs.createWriteStream(path);
 
+            const filePath = fs.createWriteStream(path);
             res.pipe(filePath);
 
             filePath.on('finish', () => {
@@ -29,7 +31,7 @@ https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) =
             })
         })
 
-
+        // falco.js
         https.get(`https://raw.githubusercontent.com/${repo}/assets/${version}/falco.js`, (res) => {
             if (res.statusCode != 200) {
                 console.error(`falco.js: ${res.statusCode} - ${res.statusMessage}`);
@@ -37,8 +39,8 @@ https.get(`https://raw.githubusercontent.com/${repo}/assets/latest.txt`, (res) =
             }
 
             const path = `src/Hooks/falco.js`;
-            const filePath = fs.createWriteStream(path);
 
+            const filePath = fs.createWriteStream(path);
             res.pipe(filePath);
 
             filePath.on('finish', () => {
