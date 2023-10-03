eed876e0e8bf16ede760ab248ff26b36a5c7e7b7
Rohith-Raju <rohithraju488@gmail.com>
2023-10-03 19:33:16 +0530

    cleanup(yaml):prevent spawning of  multiple yaml workers
    Signed-off-by: Rohith-Raju <rohithraju488@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 import monacoEditorPlugin, {
   IMonacoEditorOpts,
 } from "vite-plugin-monaco-editor";
 
 // https://vitejs.dev/config/
 
-const option: IMonacoEditorOpts = {
-  languageWorkers: [],
+const options: IMonacoEditorOpts = {
+  customWorkers: [{label:"yaml",entry:"monaco-yaml/yaml.worker.js"}],
 };
 export default defineConfig({
-  plugins: [monacoEditorPlugin.default(option), react()],
+  plugins: [monacoEditorPlugin.default(options), react()],
 });
