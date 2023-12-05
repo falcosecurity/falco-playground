f238fedcbaf36727538e1307c34a0cd0228a7954
poiana <51138685+poiana@users.noreply.github.com>
2023-12-05 17:28:26 +0000

    Deploying to gh-pages from @ falcosecurity/falco-playground@a0f665b588a3344061708f1ee29a6e2b2c854f99 🚀

--- assets/yaml-fabdb343.js
+++ assets/yaml-fabdb343.js
@@ -1,4 +1,4 @@
-import{m as i}from"./index-1c5b61b9.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./index-929ca6d9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
